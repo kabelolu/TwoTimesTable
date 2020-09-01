1c275a10b0c0d85208bd4a6f5de5d42aa01fc055
Kabelo Lucas Kekana <kabelolu86@gmail.com>
2020-09-01 12:28:47 +0200

    TimesTable

---
+++ TwoTimesTable.java
@@ -0,0 +1,14 @@
+class TwoTimesTable{
+TwoTimesTable(){}
+
+public static void main(String [] args){
+
+for(int a=1;a<=10;a=a+1){
+int result =2*a;
+if (a!=0){
+System.out.println("2 * " + a + " = " + result);
+}
+
+}
+}
+}
